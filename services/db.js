31a82a255e20658f33107b4132fe2bf7973ee8cf
Sebastian Sieber <sebastian.sieber@gmail.com>
2023-01-23 10:55:26 +0700

    remove image in docker-compose

--- services/db.js
+++ services/db.js
@@ -2,9 +2,27 @@ const mysql = require('mysql2/promise');
 const config = require('../config');
 
 async function query(sql, params) {
-    const connection = await mysql.createConnection(config.db);
-    const [results,] = await connection.execute(sql, params);
-    connection.end();
+    //const connection = await mysql.createConnection(config.db);
+    const pool = await mysql.createPool(config.db);
+
+    pool.getConnection((err, connection) => {
+        if (err) {
+            if (err.code === 'PROTOCOL_CONNECTION_LOST') {
+                console.error('Database connection was closed.')
+            }
+            if (err.code === 'ER_CON_COUNT_ERROR') {
+                console.error('Database has too many connections.')
+            }
+            if (err.code === 'ECONNREFUSED') {
+                console.error('Database connection was refused.')
+            }
+        }
+        if (connection) connection.release()
+        return
+    })
+
+    const [results,] = await pool.execute(sql, params);
+    //connection.end();
 
     return results;
 }
