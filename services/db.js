0e58a975689f896610f1ef1c7a557f7a98517a6f
Sebastian Sieber <sebastian.sieber@gmail.com>
2023-01-23 08:13:48 +0700

    close db connection

--- services/db.js
+++ services/db.js
@@ -4,6 +4,7 @@ const config = require('../config');
 async function query(sql, params) {
     const connection = await mysql.createConnection(config.db);
     const [results,] = await connection.execute(sql, params);
+    connection.end();
 
     return results;
 }
