4e3daea4660db874333722224f8a5f2984d59a7b
Sebastian Sieber <sebastian.sieber@gmail.com>
2023-01-24 16:35:59 +0700

    cleanup

--- services/db.js
+++ services/db.js
@@ -18,17 +18,3 @@ module.exports = {
     connection: async () => pool.getConnection(),
     execute: (...params) => pool.execute(...params)
 };
-
-/*
-if (err) {
-    if (err.code === 'PROTOCOL_CONNECTION_LOST') {
-        console.error('Database connection was closed.')
-    }
-    if (err.code === 'ER_CON_COUNT_ERROR') {
-        console.error('Database has too many connections.')
-    }
-    if (err.code === 'ECONNREFUSED') {
-        console.error('Database connection was refused.')
-    }
-}
-*/
