df0537c8d02d6ece1c81a261af7fa21b378aff43
Sebastian Sieber <sebastian.sieber@gmail.com>
2023-01-24 16:37:18 +0700

    try to have everything in server.js

--- server.js
+++ server.js
@@ -2,6 +2,9 @@
 const express = require('express');
 const helmet = require('helmet');
 const cors = require('cors');
+const Ajv = require('ajv');
+
+const db = require('./services/db');
 
 const PORT = process.env.APP_PORT;
 
@@ -16,8 +19,58 @@ app.use(cors({
     origin: '*'
 }));
 
-const namesRouter = require("./routes/names");
-app.use("/.well-known", namesRouter);
+app.get("/.well-known/nostr.json", async (req, res) => {
+    const schema = {
+        type: 'object',
+        properties: {
+            name: { type: 'string' }
+        },
+        required: ['name']
+    }
+
+    try {
+        const ajv = new Ajv();
+        const valid = ajv.validate(schema, req.query);
+        if (!valid) res.status(400).send(ajv.errors);
+
+        if (req.query.name) {
+            const sql = `SELECT name, pubkey FROM name WHERE name="${req.query.name}"`;
+
+            const [result] = await db.execute(sql);
+
+            if (result && result.length == 1) {
+                var resp = JSON.parse(`{
+                    "names": {
+                        "${result[0].name}": "${result[0].pubkey}"
+                    }
+                }`);
+
+                res.json(resp);
+            } else {
+                res.json({});
+            }
+        } else {
+            const sql = `SELECT name, pubkey FROM name`;
+
+            const [result] = await db.execute(sql);
+
+            let data = result.map(row => {
+                return ('\"' + row.name + '\": \"' + row.pubkey + '\"');
+            });
+
+            var resp = JSON.parse(`{
+                "names": {
+                    ${data}
+                }
+            }`);
+
+            res.json(resp);
+        }
+    } catch (err) {
+        console.error(`Error while getting names `, err.message);
+        next(err);
+    }
+});
 
 /* Error handler middleware */
 app.use((err, req, res, next) => {
